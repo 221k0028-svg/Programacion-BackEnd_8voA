2709e5bf0df930cb3aeb6627036dce620ad70d6a
221k0028-svg <221k0028@itscarrillopuerto.edu.mx>
2026-04-22 10:07:43 -0500

    Subiendo correción del codigo

--- TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index2.js
+++ TemaUno_Lenguajes/Ejercicios_1.4/Practica_1.4.3/index2.js
@@ -4,38 +4,39 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-/* import.meta.url proporciona la URL del modulo actual.
-fileURLToPath(import.meta.url) convierte esa URL en una ruta de archivo.
-dirname() recupera el nombre del directorio a partir de la ruta del archivo. */
 
 const app = express();
-var nombreEquipo = "";  
 const port = 3000;
 
-
+// Middleware para leer datos del formulario
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function registrador(req, res, next) {
-    console.log(req.body); // muestra los datos enviados por el usuario
-
-    nombreEquipo = req.body["mascota"] + req.body["adjetivo"]; // concatena los datos
-
-    next(); // pasa al siguiente middleware o ruta
-}
-
-app.use(registrador)
-
+// Ruta principal (muestra el HTML)
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/submit", (req, res) => {
+// Middleware SOLO para esta ruta
+function registrador(req, res, next) {
+    console.log("BODY:", req.body);
+
+    const nombreEquipo = req.body.mascota + " " + req.body.adjetivo;
+
+    // guardamos en req para usarlo después
+    req.nombreEquipo = nombreEquipo;
+
+    next();
+}
+
+// Ruta POST (procesa el formulario)
+app.post("/submit", registrador, (req, res) => {
     res.send(`
         <h1>El nombre de tu equipo es:</h1>
-        <h2>${nombreEquipo}</h2>
+        <h2>${req.nombreEquipo}</h2>
     `);
 });
 
+// Servidor
 app.listen(port, () => {
     console.log(`Servidor ejecutandose en el puerto ${port}`);
 });
