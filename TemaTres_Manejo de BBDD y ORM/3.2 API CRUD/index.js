ba5a1688ebcb13de07db38e162550b3ef3c2adcf
221k0028-svg <221k0028@itscarrillopuerto.edu.mx>
2026-04-28 16:39:14 -0500

    Subiendo avances AA 3.1 Proyecto CRUD API

---
+++ TemaTres_Manejo de BBDD y ORM/3.2 API CRUD/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express();
+const puerto = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a mi API CRUD 🚀');
+});
+
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en http://localhost:${puerto}`);
+});
